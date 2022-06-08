cff3c943affeefbde0a45245d6c97e43d692a32c
vkg6614 <vkg6614@gmail.com>
2022-06-08 11:29:42 +0530

    solve some error

--- server/index.js
+++ server/index.js
@@ -15,15 +15,15 @@ app.use(cors());
 
 app.use("/products", productRoute);
 
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() =>
-    app.listen(process.env.PORT, () =>
-      console.log(`server running on port ${process.env.PORT}`)
-    )
+    app.listen(PORT, () => console.log(`server running on port ${PORT}`))
   )
   .catch((error) => console.log(error.message));
 
